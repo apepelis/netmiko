b3d093dc328a5cb8c044d61ddc8663e333e72809
apepelis <aaron@pepelis.com>
2017-03-29 13:09:26 -0400

    Update extreme_ssh.py
    I think this will fix
    https://github.com/ktbyers/netmiko/issues/428

--- netmiko/extreme/extreme_ssh.py
+++ netmiko/extreme/extreme_ssh.py
@@ -1,5 +1,6 @@
 """Extreme support."""
 from __future__ import unicode_literals
+import string
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
@@ -11,3 +12,9 @@ class ExtremeSSH(CiscoSSHConnection):
         self.enable()
         self.set_base_prompt()
         self.disable_paging(command="disable clipaging\n")
+
+    def set_base_prompt(self, pri_prompt_terminator='#', alt_prompt_terminator='>', delay_factor=1):
+        super(ExtremeSSH, self).set_base_prompt(pri_prompt_terminator,alt_prompt_terminator, delay_factor)        
+        if self.base_prompt:
+            self.base_prompt = self.base_prompt.strip().rstrip(string.digits)
+        return self.base_prompt
